0bc3bd34982cbdc182a898cc36b7fc9d0823815c
Rikki Saini <rikkisaini4455@gmail.com>
2025-05-02 16:55:46 +0530

    name-check

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/school', function () {
+    return '<script>alert("Hello from About Page!");</script>';
+})->name('about');
+Route::get('/contact', function(){
+    return '<script>alert("Hello from Contact Page!");</script>';
+})->name('contact');
